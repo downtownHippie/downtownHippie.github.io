3ae3a0aececa096d4d3c471ad223872ca0c8b2db
downtownHippie <w6419986@SW-WL136-14.ad.hccs.edu>
2015-09-03 18:22:57 -0500

    initial commit

---
+++ IMED2351/week2/hello9.js
@@ -0,0 +1,9 @@
+<scripts>
+	
+	documnt.getElementByID(header1).innerHTML = "Broken JavaScript Assignment! &#9786;";
+	
+	document.getelementbyid("header2'). = "You're getting close to done";
+	
+	documents.getElementByTagNames(mycontent).innerHTML = 'Wonderful!  I'm finally fixed!';
+	
+</script>
