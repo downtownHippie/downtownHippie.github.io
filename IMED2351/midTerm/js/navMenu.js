debe2c8b51555bcd8d2f8ab7aca92f3e733ee43c
Joe Reid <jreid@vnet.net>
2015-10-25 11:31:21 -0500

    importNode ?

--- IMED2351/midTerm/js/navMenu.js
+++ IMED2351/midTerm/js/navMenu.js
@@ -23,7 +23,7 @@
 //  the original had a delay on close, I might reimplement that, it was "smoother"
 
 var navMenuImport = document.querySelector('#navMenuHTML');
-var navMenuContent = navMenuImport.import.querySelector('#navMenuList');
+var navMenuContent = navMenuImport.importNode.querySelector('#navMenuList');
 document.getElementsByTagName('nav')[0].innerHTML = document.importNode(navMenuContent, true).innerHTML;
 
 var navMenuMenus = document.getElementsByClassName("navMenu");
