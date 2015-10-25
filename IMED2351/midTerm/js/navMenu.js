709fcdc8944eee9e63b08f1d26081873f22eb41b
Joe trying to be mobily enlightened <jreid@vnet.net>
2015-10-25 09:19:07 -0500

    initial commit to import the navmenu

--- IMED2351/midTerm/js/navMenu.js
+++ IMED2351/midTerm/js/navMenu.js
@@ -22,6 +22,10 @@
 //
 //  the original had a delay on close, I might reimplement that, it was "smoother"
 
+var navMenuImport = document.querySelector('#navMenuHTML');
+var navMenuContet = navMenuImport.import.querySelector('nav');
+document.getElementsByTagName("nav")[0].innerHTML = document.importNode(navMenuContent, true);
+
 var navMenuMenus = document.getElementsByClassName("navMenu");
 var navMenuMenuHeaders = document.getElementsByClassName("navMenuHeader");
 
